c0834c33f16822e5922b7a512bf9b0f087d10bd1
AlexCatze <alexcatze@gmail.com>
2023-05-08 19:10:21 +0300

    Use mixin instead of AT

---
+++ src/main/kotlin/alexsocol/asjlib/mixins/GameDataMixin.java
@@ -0,0 +1,17 @@
+package alexsocol.asjlib.mixins;
+
+import cpw.mods.fml.common.registry.GameData;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(GameData.class)
+public interface GameDataMixin  {
+    @Invoker("getMain")
+    static GameData invokeGetMain() {
+        throw new AssertionError();
+    }
+
+    @Invoker("registerItem")
+    GameData invokeRegisterItem(Item item, String name, int idHint);
+}
