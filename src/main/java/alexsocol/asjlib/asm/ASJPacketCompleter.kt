119dd395b229cbb193ddc9b129eeb67c52b7e0d6
danyadev <danyadev@mail.ru>
2026-03-22 11:44:56 +0300

    don't compute ClassNode in ASJPacketCompleter.transform when not used
    ASJPacketCompleter time: 1450ms -> 700ms

--- src/main/java/alexsocol/asjlib/asm/ASJPacketCompleter.kt
+++ src/main/java/alexsocol/asjlib/asm/ASJPacketCompleter.kt
@@ -10,47 +10,45 @@ import org.objectweb.asm.tree.*
 class ASJPacketCompleter: ASJAbstractClassTransformer() {
 	
 	override fun transform(transformedName: String, basicClass: ByteArray): ByteArray {
+		if (!isASJPacket(basicClass)) return basicClass;
+
 		try {
 			val cr = ClassReader(basicClass)
 			val cn = ClassNode()
 			cr.accept(cn, 0)
-			
-			if (isASJPacket(basicClass)) {
-				logger.debug("Expanding ASJPacket $transformedName")
-				val fs = BooleanArray(5) // <init>, fromBytes, toBytes, fromCustomBytes, toCustomBytes
-				for (mt in cn.methods) {
-					if (mt.name == "<init>" && mt.desc == "()V") {
-						fs[0] = true
-					} else
-					if (mt.name == "fromBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
-						fs[1] = true
-					} else
-					if (mt.name == "toBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
-						fs[2] = true
-					} else
-					if (mt.name == "fromCustomBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
-						fs[3] = true
-					} else
-					if (mt.name == "toCustomBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
-						fs[4] = true
-					}
+
+			logger.debug("Expanding ASJPacket $transformedName")
+			val fs = BooleanArray(5) // <init>, fromBytes, toBytes, fromCustomBytes, toCustomBytes
+			for (mt in cn.methods) {
+				if (mt.name == "<init>" && mt.desc == "()V") {
+					fs[0] = true
+				} else
+				if (mt.name == "fromBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
+					fs[1] = true
+				} else
+				if (mt.name == "toBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
+					fs[2] = true
+				} else
+				if (mt.name == "fromCustomBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
+					fs[3] = true
+				} else
+				if (mt.name == "toCustomBytes" && mt.desc == "(Lio/netty/buffer/ByteBuf;)V") {
+					fs[4] = true
 				}
-				
-				if (!fs[0]) makeConstructor(cn)
-				if (!fs[1]) makeFromBytes(cn, fs[3])
-				if (!fs[2]) makeToBytes(cn, fs[4])
-				
-				val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES or ClassWriter.COMPUTE_MAXS)
-				cn.accept(cw)
-				
-				return cw.toByteArray()
 			}
+
+			if (!fs[0]) makeConstructor(cn)
+			if (!fs[1]) makeFromBytes(cn, fs[3])
+			if (!fs[2]) makeToBytes(cn, fs[4])
+
+			val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES or ClassWriter.COMPUTE_MAXS)
+			cn.accept(cw)
+
+			return cw.toByteArray()
 		} catch (e: Throwable) {
 			logger.error("Something went wrong while transforming class $transformedName. Ignore if everything is OK (this is NOT ASJCore error):", e)
-			
-			return basicClass
 		}
-		
+
 		return basicClass
 	}
 	
